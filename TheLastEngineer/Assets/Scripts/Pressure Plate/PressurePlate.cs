8a6f4da71ec9e5f3c1aa5db829ba62fa7794c4fa
matiasbenavide <matiasbenavide@gmail.com>
2025-04-09 18:39:10 -0300

    Corrijo feedback de Pressure Plates System

--- TheLastEngineer/Assets/Scripts/Pressure Plate/PressurePlate.cs
+++ TheLastEngineer/Assets/Scripts/Pressure Plate/PressurePlate.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    [SerializeField] private PressuredDoor _pressureDoor;
     [SerializeField] private Material _commonMat;
     [SerializeField] private Material _successMat;
     [SerializeField] private float _pressedCD;
-
+    
     private MeshRenderer _renderer;
+    private bool _taskFinished = false;
     
     public Action<bool> OnPlayerPressed;
 
@@ -16,21 +18,56 @@ public class PressurePlate : MonoBehaviour
     {
         _renderer = GetComponent<MeshRenderer>();
         _renderer.material = _commonMat;
+
+        _pressureDoor.OnTaskFinished += TaskFinished;
+    }
+
+    private void Update()
+    {
+        if (_taskFinished)
+        {
+            _renderer.material = _successMat;
+        }
+    }
+
+    private void TaskFinished(bool taskFinished)
+    {
+        _taskFinished = taskFinished;
+        
+        if (_taskFinished)
+        {
+            _renderer.material = _successMat;
+        }
+        else
+        {
+            _renderer.material = _commonMat;
+            OnPlayerPressed?.Invoke(false);
+        }
     }
 
     private IEnumerator PressedCD()
     {
+        _renderer.material = _successMat;
+        OnPlayerPressed?.Invoke(true);
+
         yield return new WaitForSeconds(_pressedCD);
+            
         _renderer.material = _commonMat;
-        OnPlayerPressed(false);
+        OnPlayerPressed?.Invoke(false);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider coll)
     {
-        if (other.GetComponent<PlayerTDController>())
+        if (coll.GetComponent<PlayerTDController>() && !_taskFinished)
         {
-            OnPlayerPressed(true);
             _renderer.material = _successMat;
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.GetComponent<PlayerTDController>() && !_taskFinished)
+        {
             StartCoroutine(PressedCD());
         }
     }
--- TheLastEngineer/Assets/Scripts/Pressure Plate/PressuredDoor.cs
+++ TheLastEngineer/Assets/Scripts/Pressure Plate/PressuredDoor.cs
@@ -1,13 +1,20 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
 
 public class PressuredDoor : MonoBehaviour
 {
     [SerializeField] private Material _commonMat;
     [SerializeField] private Material _successMat;
     [SerializeField] private PressurePlate[] _plates;
+    [SerializeField] private float _restartCD;
 
     private MeshRenderer _renderer;
     private int _pressedPlates = 0;
+    private bool _taskFinished = false;
+
+    public Action<bool> OnTaskFinished;
 
     void Start()
     {
@@ -22,11 +29,14 @@ public class PressuredDoor : MonoBehaviour
 
     void Update()
     {
-        if (_pressedPlates == _plates.Length)
+        Debug.Log("Door :" + _taskFinished);
+
+        if (_pressedPlates == _plates.Length && !_taskFinished)
         {
-            _renderer.material = _successMat;
+            OnTaskFinished?.Invoke(true);
+            StartCoroutine(RestartTask());
         }
-        else if (_pressedPlates == 0)
+        else if(_renderer.material != _commonMat && !_taskFinished)
         {
             _renderer.material = _commonMat;
         }
@@ -41,6 +51,22 @@ public class PressuredDoor : MonoBehaviour
         else
         {
             _pressedPlates--;
+            
+            if (_pressedPlates < 0) _pressedPlates = 0;
         }
+
+        Debug.Log("PRESSED PLATES: " + _pressedPlates);
+    }
+
+    private IEnumerator RestartTask()
+    {
+        _taskFinished = true;
+        _renderer.material = _successMat;
+        
+        yield return new WaitForSeconds(_restartCD);
+        
+        _taskFinished = false;
+        _renderer.material = _commonMat;
+        OnTaskFinished?.Invoke(false);
     }
 }
