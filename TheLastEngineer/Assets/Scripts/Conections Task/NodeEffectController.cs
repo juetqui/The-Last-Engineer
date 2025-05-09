66468e4e9047094ec7645a3694c2db900f303f13
Julián Lionel Etchevarne Quimbel <60628743+juetqui@users.noreply.github.com>
2025-05-09 15:37:34 -0300

    lvl 1 y 2  del cubo escudo terminado

--- TheLastEngineer/Assets/Scripts/Conections Task/NodeEffectController.cs
+++ TheLastEngineer/Assets/Scripts/Conections Task/NodeEffectController.cs
@@ -1,11 +1,11 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class NodeEffectController : MonoBehaviour
 {
     [SerializeField] private NodeType _requiredType;
-    [SerializeField] private List<Transform> _objectsToMove;
+    [SerializeField] private List<GameObject> _objectsToMove;
     [SerializeField] private List<Transform> _newPositions;
     [SerializeField] private float _moveSpeed;
 
@@ -23,8 +23,8 @@ public class NodeEffectController : MonoBehaviour
 
         foreach (var obj in _objectsToMove)
         {
-            _originalPositions.Add(obj.position);
-            _originalRotations.Add(obj.rotation);
+            _originalPositions.Add(obj.transform.position);
+            _originalRotations.Add(obj.transform.rotation);
         }
     }
 
@@ -58,27 +58,7 @@ public class NodeEffectController : MonoBehaviour
 
     private void MoveObjects()
     {
-        for (int i = 0; i < _objectsToMove.Count; i++)
-        {
-            if (i >= _newPositions.Count || _objectsToMove[i] == null || _newPositions[i] == null)
-                continue;
-
-            float initialDistance = Vector3.Distance(_originalPositions[i], _newPositions[i].position);
-            float currentDistance = Vector3.Distance(_objectsToMove[i].position, _newPositions[i].position);
-            float t = initialDistance > 0.001f ? 1f - (currentDistance / initialDistance) : 1f;
-
-            _objectsToMove[i].position = Vector3.MoveTowards(
-                _objectsToMove[i].position,
-                _newPositions[i].position,
-                Time.deltaTime * _moveSpeed
-            );
-
-            _objectsToMove[i].rotation = Quaternion.Slerp(
-                _originalRotations[i],
-                _newPositions[i].rotation,
-                t
-            );
-        }
+        StartCoroutine(TagsPositions());
     }
 
     private void ResetObjects()
@@ -89,20 +69,53 @@ public class NodeEffectController : MonoBehaviour
                 continue;
 
             float initialDistance = Vector3.Distance(_newPositions[i].position, _originalPositions[i]);
-            float currentDistance = Vector3.Distance(_objectsToMove[i].position, _originalPositions[i]);
+            float currentDistance = Vector3.Distance(_objectsToMove[i].transform.position, _originalPositions[i]);
             float t = initialDistance > 0.001f ? 1f - (currentDistance / initialDistance) : 1f;
 
-            _objectsToMove[i].position = Vector3.MoveTowards(
-                _objectsToMove[i].position,
+            _objectsToMove[i].transform.position = Vector3.MoveTowards(
+                _objectsToMove[i].transform.position,
                 _originalPositions[i],
                 Time.deltaTime * _moveSpeed
             );
 
-            _objectsToMove[i].rotation = Quaternion.Slerp(
+            _objectsToMove[i].transform.rotation = Quaternion.Slerp(
                 _newPositions[i].rotation,
                 _originalRotations[i],
                 t
             );
+        }
+    }
+
+    private IEnumerator TagsPositions() 
+    {
+        for (int i = 0; i < _objectsToMove.Count; i++)
+        {
+            if (i >= _newPositions.Count || _objectsToMove[i] == null || _newPositions[i] == null)
+                continue;
+
+            string tag = _objectsToMove[i].tag;
+            _objectsToMove[i].tag = "Untagged";
+
+
+            float initialDistance = Vector3.Distance(_originalPositions[i], _newPositions[i].position);
+            float currentDistance = Vector3.Distance(_objectsToMove[i].transform.position, _newPositions[i].position);
+            float t = initialDistance > 0.001f ? 1f - (currentDistance / initialDistance) : 1f;
+
+            _objectsToMove[i].transform.position = Vector3.MoveTowards(
+                _objectsToMove[i].transform.position,
+                _newPositions[i].position,
+                Time.deltaTime * _moveSpeed
+            );
+
+            _objectsToMove[i].transform.rotation = Quaternion.Slerp(
+                _originalRotations[i],
+                _newPositions[i].rotation,
+                t
+            );
+
+            _objectsToMove[i].tag = tag;
+
+            yield return null;
         }
     }
 }
