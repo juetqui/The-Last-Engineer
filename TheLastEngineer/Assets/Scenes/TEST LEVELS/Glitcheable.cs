f1bf3524d4f9a327944523210f5ac06927088b90
matiasbenavide <matiasbenavide@gmail.com>
2025-06-01 16:15:53 -0300

    Agrego nueva mecanica para el movimiento de objetos

---
+++ TheLastEngineer/Assets/Scenes/TEST LEVELS/Glitcheable.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Glitcheable : MonoBehaviour
+{
+    //[SerializeField] private Camera _cam;
+    [SerializeField] private Transform _newPos;
+    //[SerializeField] private float _defaultDuration = 1f;
+    //[SerializeField] private float _nodeDuration = 2f;
+
+    private Vector3 _originalPos = default, _targetPos = default;
+    //private Image _timer = default;
+    private bool _disposed = false;
+    //private bool _disposed = false, _canMove = true, _isStopped = false;
+    //private float _currentDuration = default;
+
+    private void Awake()
+    {
+        _targetPos = _newPos.position;
+        _originalPos = transform.position;
+        //_currentDuration = _defaultDuration;
+    }
+
+    void Start()
+    {
+        //PlayerTDController.Instance.OnNodeGrabed += CheckNode;
+        MoveControl.Instance.OnChangePositions += UpdateTarget;
+    }
+
+    //void Update()
+    //{
+    //    if (_canMove && !_isStopped)
+    //    {
+    //        StartCoroutine(StartTimer());
+    //    }
+    //}
+
+    //private void CheckNode(bool hasNode, NodeType nodeType)
+    //{
+    //    _currentDuration = (!hasNode || nodeType != NodeType.Green) ? _defaultDuration : _nodeDuration;
+    //}
+
+    private void UpdateTarget()
+    {
+        transform.position = _targetPos;
+
+        _disposed = !_disposed;
+        _targetPos = _disposed ? _originalPos : _newPos.position;
+    }
+
+    //private IEnumerator StartTimer()
+    //{
+    //    _canMove = false;
+
+    //    while (_timer.fillAmount > 0f)
+    //    {
+    //        _timer.fillAmount -= _currentDuration * Time.deltaTime;
+    //        yield return null;
+    //    }
+
+    //    _timer.fillAmount = 0f;
+
+    //    yield return new WaitForSeconds(0.25f);
+
+    //    transform.position = _targetPos;
+    //    UpdateTarget();
+
+    //    _timer.fillAmount = 1f;
+    //    _canMove = true;
+    //}
+}
---
+++ TheLastEngineer/Assets/Scenes/TEST LEVELS/MoveControl.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveControl : MonoBehaviour
+{
+    public static MoveControl Instance = null;
+
+    [SerializeField] private float _defaultDuration = 1f;
+    [SerializeField] private float _nodeDuration = 2f;
+
+    private Image _timer = default;
+    private bool _canMove = true, _isStopped = false;
+    private float _currentDuration = default;
+
+    public Action OnChangePositions = delegate { };
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+
+        _timer = GetComponentInChildren<Image>();
+        _timer.fillAmount = 1f;
+
+        _currentDuration = _defaultDuration;
+    }
+
+    void Start()
+    {
+        PlayerTDController.Instance.OnNodeGrabed += CheckNode;
+    }
+
+    void Update()
+    {
+        if (_canMove && !_isStopped)
+        {
+            StartCoroutine(StartTimer());
+        }
+    }
+
+    private void CheckNode(bool hasNode, NodeType nodeType)
+    {
+        _currentDuration = (!hasNode || nodeType != NodeType.Green) ? _defaultDuration : _nodeDuration;
+    }
+
+    private IEnumerator StartTimer()
+    {
+        _canMove = false;
+
+        while (_timer.fillAmount > 0f)
+        {
+            _timer.fillAmount -= _currentDuration * Time.deltaTime;
+            yield return null;
+        }
+
+        _timer.fillAmount = 0f;
+
+        yield return new WaitForSeconds(0.25f);
+
+        OnChangePositions?.Invoke();
+
+        _timer.fillAmount = 1f;
+        _canMove = true;
+    }
+}
