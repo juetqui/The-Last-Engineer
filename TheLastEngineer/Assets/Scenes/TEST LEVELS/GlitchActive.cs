09dd255500c6769ae756a61b38654e0eef52e9bd
matiasbenavide <matiasbenavide@gmail.com>
2025-06-05 18:28:56 -0300

    Agrego prototipo de LockOn

--- TheLastEngineer/Assets/Scenes/TEST LEVELS/GlitchActive.cs
+++ TheLastEngineer/Assets/Scenes/TEST LEVELS/GlitchActive.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GlitchActive : MonoBehaviour
@@ -12,7 +14,9 @@ public class GlitchActive : MonoBehaviour
     [SerializeField] private GameObject _interactionArea;
 
     private PlayerTDController _player = default;
-    private float _detectionOffset = 2f, _offsetRange = 0f;
+    private List<Glitcheable> _glitcheables = default;
+    private float _detectionOffset = 5f, _offsetRange = 0f;
+    private int _index = 0;
     private bool _enabled = false;
 
     public Action<Glitcheable> OnStopObject = delegate { };
@@ -25,6 +29,7 @@ public class GlitchActive : MonoBehaviour
 
         _player = GetComponent<PlayerTDController>();
         _offsetRange = _detectionRange + _detectionOffset;
+        _glitcheables = new List<Glitcheable>();
     }
 
     void Start()
@@ -36,15 +41,36 @@ public class GlitchActive : MonoBehaviour
     {
         if (_enabled)
         {
-            Glitcheable glitcheable = GetNearestStopable();
-
+            _glitcheables = GetStopablesInArea(ref _glitcheables);
+            Glitcheable glitcheable = _glitcheables.Count > 0 ? _glitcheables[_index] : _glitcheables.FirstOrDefault();
             OnStopableSelected?.Invoke(glitcheable);
 
-            if (Input.GetKeyDown(KeyCode.V) && glitcheable != null)
+            if (Input.GetKeyDown(KeyCode.I) && _glitcheables.Count > 0)
             {
-                OnStopObject?.Invoke(glitcheable);
-                Debug.Log("HOLA");
+                if (_index >= _glitcheables.Count - 1)
+                {
+                    _index = 0;
+                }
+                else
+                {
+                    _index++;
+                }
             }
+            
+            if (Input.GetKeyDown(KeyCode.U) && _glitcheables.Count > 0)
+            {
+                if (_index <= 0)
+                {
+                    _index = _glitcheables.Count - 1;
+                }
+                else
+                {
+                    _index--;
+                }
+            }
+            
+            if (Input.GetKeyDown(KeyCode.V) && glitcheable != null)
+                OnStopObject?.Invoke(glitcheable);
         }
     }
 
@@ -52,14 +78,14 @@ public class GlitchActive : MonoBehaviour
     {
         if (!hasNode || nodeType != _requiredNode)
         {
+            OnStopableSelected(null);
+            _index = 0;
             StartCoroutine(DeactivateArea());
             return;
         }
 
         StartCoroutine(ActivateArea());
-        _enabled = true;
     }
-
     private Glitcheable GetNearestStopable()
     {
         Collider[] hitColliders = Physics.OverlapSphere(_player.transform.position, _detectionRange);
@@ -82,6 +108,23 @@ public class GlitchActive : MonoBehaviour
         return closest;
     }
 
+    private List<Glitcheable> GetStopablesInArea(ref List<Glitcheable> glitcheables)
+    {
+        glitcheables.Clear();
+        
+        Collider[] hitColliders = Physics.OverlapSphere(_player.transform.position, _detectionRange);
+
+        foreach (var hit in hitColliders)
+        {
+            if (hit.TryGetComponent(out Glitcheable glitcheable))
+            {
+                glitcheables.Add(glitcheable);
+            }
+        }
+
+        return glitcheables.OrderBy(glitch => Vector3.Distance(_player.transform.position, glitch.transform.position)).ToList();
+    }
+
     private IEnumerator ActivateArea()
     {
         while (_interactionArea.transform.localScale.x < _offsetRange)
@@ -102,7 +145,7 @@ public class GlitchActive : MonoBehaviour
 
         while (_interactionArea.transform.localScale.x > 0.1f)
         {
-            Vector3 newScale = new Vector3(5f, 0f, 5f);
+            Vector3 newScale = new Vector3(_scaleSpeed, 0f, _scaleSpeed);
 
             _interactionArea.transform.localScale -= newScale * Time.deltaTime;
             yield return null;
