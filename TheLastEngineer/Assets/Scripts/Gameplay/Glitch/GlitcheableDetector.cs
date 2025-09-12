b98abeb6c825032ae358bc2ae2551069fb134479
matiasbenavide <matiasbenavide@gmail.com>
2025-09-11 21:00:48 -0300

    Remuevo herencia de Monobehaviour en GlitcheableDetector

--- TheLastEngineer/Assets/Scripts/Gameplay/Glitch/GlitcheableDetector.cs
+++ TheLastEngineer/Assets/Scripts/Gameplay/Glitch/GlitcheableDetector.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GlitcheableDetector : MonoBehaviour
+public class GlitcheableDetector
 {
     public GlitcheableDetector(float detectionRange, LayerMask selectionLayer)
     {
