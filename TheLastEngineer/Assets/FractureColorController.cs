b82fc531a793ffc1ae08f1b7e398ba742da30189
matiasbenavide <matiasbenavide@gmail.com>
2025-09-17 18:32:31 -0300

    Comienzo shader de humo, modifico Fracture shader y le agrego un script para manejar sus valores

---
+++ TheLastEngineer/Assets/FractureColorController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureColorController : MonoBehaviour
+{
+    [SerializeField] private Transform _parent;
+    [SerializeField] private List<Color> _colors;
+
+    private Renderer _renderer = default;
+
+    void Start()
+    {
+        _colors = new List<Color>();
+        _colors.Add(Color.white);
+        _colors.Add(Color.black);
+        _colors.Add(Color.magenta);
+
+        _renderer = GetComponent<Renderer>();
+
+        int randomColor = Random.Range(0, _colors.Count);
+
+        _renderer.material.SetColor("_MainColor", _colors[randomColor]);
+        _renderer.material.SetVector("_Parent", _parent.position);
+    }
+
+}
