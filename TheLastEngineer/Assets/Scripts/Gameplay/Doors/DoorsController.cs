1adfd5e5436df938db53419f15e7504a359cd75f
Julián Lionel Etchevarne Quimbel <60628743+juetqui@users.noreply.github.com>
2025-09-18 19:22:39 -0300

    mejoras lvl1

--- TheLastEngineer/Assets/Scripts/Gameplay/Doors/DoorsController.cs
+++ TheLastEngineer/Assets/Scripts/Gameplay/Doors/DoorsController.cs
@@ -1,41 +1,43 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    [Header("Referencias")]
-    [SerializeField] private DoorsView _door;
     [SerializeField] private List<Connection> _connections = new List<Connection>();
 
-    // Runtime
+    private DoorsView _door;
     private int _activeCount = 0;
     private bool _isOpen = false, _shouldOpen;
 
-    private void OnEnable()
-    {
-        Subscribe(true);
-    }
-
-    private void OnDisable()
-    {
-        Subscribe(false);
-    }
-
     private void Start()
     {
-        // IMPORTANTE: Puede haber condiciones de orden de ejecución con Connection.Start().
-        // Para estar seguros de leer el estado real, inicializamos al final del primer frame.
-        StartCoroutine(DelayedInit());
-    }
-
-    private IEnumerator DelayedInit()
-    {
-        yield return null; // espera 1 frame
+        _door = GetComponent<DoorsView>();
+        EvaluateAndApply();
         RecountActiveConnections();
+    }
+    private void EvaluateAndApply()
+    {
+        _shouldOpen = (_activeCount == _connections.Count && _connections.Count > 0);
+
+        if (_shouldOpen == _isOpen) return;
+
+        _isOpen = _shouldOpen;
+        _door.OpenDoor(_isOpen);
+    }
+    private void RecountActiveConnections()
+    {
+        _activeCount = 0;
+        foreach (var c in _connections)
+        {
+            if (c != null && c.IsConnected) _activeCount++;
+        }
+    }
+    private void OnConnectionStateChanged(NodeType type, bool connected)
+    {
+        if(type == NodeType.Default) _activeCount += connected ? 1 : -1;
+        _activeCount = Mathf.Clamp(_activeCount, 0, _connections.Count);
         EvaluateAndApply();
     }
-
     private void Subscribe(bool subscribe)
     {
         if (_connections == null) return;
@@ -50,30 +52,12 @@ public class DoorController : MonoBehaviour
                 c.OnNodeConnected -= OnConnectionStateChanged;
         }
     }
-
-    private void OnConnectionStateChanged(NodeType type, bool connected)
+    private void OnEnable() 
     {
-        if(type == NodeType.Default) _activeCount += connected ? 1 : -1;
-        _activeCount = Mathf.Clamp(_activeCount, 0, _connections.Count);
-        EvaluateAndApply();
+        Subscribe(true);
     }
-
-    private void RecountActiveConnections()
+    private void OnDisable()
     {
-        _activeCount = 0;
-        foreach (var c in _connections)
-        {
-            if (c != null && c.IsConnected) _activeCount++;
-        }
-    }
-
-    private void EvaluateAndApply()
-    {
-        _shouldOpen = (_activeCount == _connections.Count && _connections.Count > 0);
-
-        if (_shouldOpen == _isOpen) return;
-
-        _isOpen = _shouldOpen;
-        _door.OpenDoor(_isOpen);
+        Subscribe(false);
     }
 }
