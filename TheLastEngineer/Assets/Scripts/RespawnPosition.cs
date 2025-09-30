c27291e2cb4825a3c6f07911ea838a00e172f2bd
matiasbenavide <matiasbenavide@gmail.com>
2025-09-30 17:27:14 -0300

    cls

--- TheLastEngineer/Assets/Scripts/RespawnPosition.cs
+++ TheLastEngineer/Assets/Scripts/RespawnPosition.cs
@@ -1,26 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RespawnPosition : MonoBehaviour
 {
-    [SerializeField] Transform _respawnPos;
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PlayerController> (out PlayerController playerController))
-        {
-            playerController.SetCheckPointPos(_respawnPos.position);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            playerController.SetCheckPointPos(transform.position);
     }
 }
