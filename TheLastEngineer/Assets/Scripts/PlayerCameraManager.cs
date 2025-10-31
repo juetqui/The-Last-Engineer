a91c25500105ee06c129c63b7ffbbaf3f28af7a0
matiasbenavide <matiasbenavide@gmail.com>
2025-10-31 17:03:59 -0300

    Agrego Timeline para animacion de ascensor en inicio del HUB, modifico particulas FallingRocks y corrijo materiales con emissive nuevo

--- TheLastEngineer/Assets/Scripts/PlayerCameraManager.cs
+++ TheLastEngineer/Assets/Scripts/PlayerCameraManager.cs
@@ -7,7 +7,10 @@ public class PlayerCameraManager : MonoBehaviour
     [SerializeField] private Collider _trigger;
     [SerializeField] private LeanTweenType _tweenType;
     [SerializeField] private float _duration;
-
+    
+    private IMovablePassenger _player = null;
+    private Vector3 _lastPosition = Vector3.zero;
+    private bool _hasPlayerOn = false;
     private float _startHeight = 0f, _targetHeight = 30f;
 
     private void Start()
@@ -15,7 +18,37 @@ public class PlayerCameraManager : MonoBehaviour
         _startHeight = _camera.m_Orbits[1].m_Height;
     }
 
+    private void LateUpdate()
+    {
+        if (!_hasPlayerOn || _player == null)
+            return;
+
+        Vector3 displacement = transform.position - _lastPosition;
+
+        if (displacement.sqrMagnitude > 0.0001f)
+            _player.OnPlatformMoving(displacement);
+
+        _lastPosition = transform.position;
+    }
+
     private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.TryGetComponent(out PlayerController player))
+        {
+            _player = player;
+            _hasPlayerOn = true;
+            _lastPosition = transform.position;
+
+            LeanTween.value(gameObject, _targetHeight, _startHeight, _duration)
+                .setEase(_tweenType)
+                .setOnUpdate((float value) =>
+                {
+                    _camera.m_Orbits[1].m_Height = value;
+                });
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
     {
         if (coll.TryGetComponent(out PlayerController player))
         {
@@ -24,10 +57,6 @@ public class PlayerCameraManager : MonoBehaviour
                 .setOnUpdate((float value) =>
                 {
                     _camera.m_Orbits[1].m_Height = value;
-                })
-                .setOnComplete(() =>
-                {
-                    _trigger.enabled = false;
                 });
         }
     }
