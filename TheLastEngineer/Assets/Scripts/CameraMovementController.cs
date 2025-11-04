bd3d3fd8b95028db381ea35063c5c97846802713
matiasbenavide <matiasbenavide@gmail.com>
2025-11-04 15:36:30 -0300

    Mejoro funcionalidad de manejo de camara con stick derecho

--- TheLastEngineer/Assets/Scripts/CameraMovementController.cs
+++ TheLastEngineer/Assets/Scripts/CameraMovementController.cs
@@ -6,6 +6,7 @@ public class CameraMovementController : MonoBehaviour
 {
     [Header("Movement Sensitivity")]
     [SerializeField] private float _sensitivity = 2f;
+    [SerializeField] private LeanTweenType _easeType;
 
     [Header("Offset Limits")]
     [SerializeField] private float _XLimit = 5f;
@@ -27,53 +28,54 @@ public class CameraMovementController : MonoBehaviour
 
     private void Start()
     {
-        InputManager.Instance.rotateInput.started += OnLook;
         InputManager.Instance.rotateInput.performed += OnLook;
         InputManager.Instance.rotateInput.canceled += OnReset;
-
         InputManager.Instance.resetCamInput.performed += OnReset;
+
+        OnReset();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         if (_isResetting) return;
 
         if (_inputOffset != Vector2.zero)
-        {
-            _currentOffset.x += _inputOffset.x * _sensitivity * Time.deltaTime;
-            _currentOffset.y += _inputOffset.y * _sensitivity * Time.deltaTime;
-
-            _currentOffset.x = Mathf.Clamp(_currentOffset.x, -_XLimit, _XLimit);
-            _currentOffset.y = Mathf.Clamp(_currentOffset.y, -_YLimit, _YLimit);
-
-            _composer.m_TrackedObjectOffset = _currentOffset;
-        }
+            SetOffset();
     }
 
-    public void OnLook(InputAction.CallbackContext context)
+    private void OnLook(InputAction.CallbackContext context)
     {
-        _inputOffset = context.ReadValue<Vector2>();
+        _inputOffset = context.ReadValue<Vector2>().normalized;
     }
 
-    public void OnReset(InputAction.CallbackContext context)
+    private void OnReset(InputAction.CallbackContext context = default)
     {
-        if (context.performed)
-        {
-            _isResetting = true;
+        _inputOffset = Vector2.zero;
+        _isResetting = true;
 
-            LeanTween.value(gameObject, _currentOffset, Vector3.zero, 0.5f)
-                .setEaseOutCubic()
-                .setOnUpdate((Vector3 valor) =>
-                {
-                    _currentOffset = valor;
-                    _composer.m_TrackedObjectOffset = _currentOffset;
-                })
-                .setOnComplete(() =>
-                {
-                    _currentOffset = Vector3.zero;
-                    _composer.m_TrackedObjectOffset = _currentOffset;
-                    _isResetting = false;
-                });
-        }
+        LeanTween.value(gameObject, _currentOffset, Vector3.zero, 0.5f)
+            .setEase(_easeType)
+            .setOnUpdate((Vector3 valor) =>
+            {
+                _currentOffset = valor;
+                _composer.m_TrackedObjectOffset = _currentOffset;
+            })
+            .setOnComplete(() =>
+            {
+                _currentOffset = Vector3.zero;
+                _composer.m_TrackedObjectOffset = _currentOffset;
+                _isResetting = false;
+            });
+    }
+
+    private void SetOffset()
+    {
+        _currentOffset.x += _inputOffset.x * _sensitivity * Time.deltaTime;
+        _currentOffset.y += _inputOffset.y * _sensitivity * Time.deltaTime;
+
+        _currentOffset.x = Mathf.Clamp(_currentOffset.x, -_XLimit, _XLimit);
+        _currentOffset.y = Mathf.Clamp(_currentOffset.y, -_YLimit, _YLimit);
+
+        _composer.m_TrackedObjectOffset = _currentOffset;
     }
 }
