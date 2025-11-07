8ae387b1efde059a35d707e75a7e3e4c40db526d
matiasbenavide <matiasbenavide@gmail.com>
2025-11-07 18:22:38 -0300

    Agrego prefab para menu de pausa funcional

---
+++ TheLastEngineer/Assets/Scripts/UI/PauseGameController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseGameController : MonoBehaviour
+{
+    private Canvas _canvas = default;
+    private bool _isPaused = false;
+
+    private void Start()
+    {
+        _canvas = GetComponent<Canvas>();
+        _canvas.enabled = false;
+
+        InputManager.Instance.pauseInput.started += PauseGame;
+    }
+
+    private void PauseGame(InputAction.CallbackContext context)
+    {
+        _isPaused = !_isPaused;
+        SetTimescale(_isPaused);
+    }
+
+    public void ResumeGame()
+    {
+        _isPaused = false;
+        SetTimescale(_isPaused);
+    }
+
+    public void GoToScene(string scene)
+    {
+        SetTimescale(false);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void GoToDesktop()
+    {
+        Application.Quit();
+    }
+
+    private void SetTimescale(bool isPaused)
+    {
+        Time.timeScale = isPaused ? 0.0f : 1.0f;
+        _canvas.enabled = isPaused;
+    }
+}
